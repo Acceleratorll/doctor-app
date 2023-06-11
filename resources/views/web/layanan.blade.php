9d234c11169445e58bb1014521664ca7dd042c99
rizkyygunawan03 <rggunawan93@gmail.com>
2023-06-12 03:18:48 +0700

    file web homepage

---
+++ resources/views/web/layanan.blade.php
@@ -0,0 +1,331 @@
+@extends('layouts.mainweb')
+
+@section('content')
+        
+<!-- =========================
+            Header
+        =========================== -->
+    <header class="header header-layout1">
+        <!-- /.header-top -->
+        <nav class="navbar navbar-expand-lg sticky-navbar">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="{{ url('/home') }}">
+                <img src="assets/images/logo/2.png" class="logo-light" alt="logo">
+                <img src="assets/images/logo/1.png" class="logo-dark" alt="logo">
+            </a>
+            <button class="navbar-toggler" type="button">
+                <span class="menu-lines"><span></span></span>
+            </button>
+            <div class="collapse navbar-collapse" id="mainNavigation">
+                <ul class="navbar-nav ml-auto">
+                <li class="nav__item">
+                    <a href="{{ url('/home') }}" class="nav__item-link">Home</a>
+                </li>
+                <!-- /.nav-item -->
+                <li class="nav__item">
+                    <a href="{{ url('/layanan') }}" class="nav__item-link active">Layanan</a>
+                </li><!-- /.nav-item -->
+                <li class="nav__item">
+                    <a href="{{ url('/profile') }}" class="nav__item-link">Profile Doctors</a>
+                </li><!-- /.nav-item -->
+                <li class="nav__item">
+                    <a href="{{ url('/contact') }}" class="nav__item-link">Contacts Us</a>
+                </li><!-- /.nav-item -->
+                </ul><!-- /.navbar-nav -->
+                <button class="close-mobile-menu d-block d-lg-none"><i class="fas fa-times"></i></button>
+            </div><!-- /.navbar-collapse -->
+            <div class="d-none d-xl-flex align-items-center position-relative ml-30">
+                <a href="{{ url('/login') }}" class="btn btn__primary btn__rounded ml-30">
+                <i class="icon-calendar"></i>
+                <span>Login</span>
+                </a>
+            </div>
+            </div><!-- /.container -->
+        </nav><!-- /.navabr -->
+        </header><!-- /.Header -->
+
+        <!-- ============================
+            Slider
+        ============================== -->
+        <section class="slider slider-centerd"
+        <div class="slick-carousel m-slides-0 carousel-arrows-light carousel-dots-light"
+            data-slick='{"slidesToShow": 1, "arrows": true, "dots": false, "speed": 700,"fade": true,"cssEase": "linear"}'>
+            <div class="slide-item align-v-h">
+            <div class="bg-img"><img src="assets/images/sliders/12.png" alt="slide img"></div>
+            <div class="container">
+                <div class="row align-items-center">
+                <div class="col-sm-12 col-md-12 col-lg-12 col-xl-8 offset-xl-2">
+                    <div class="slide__content">
+                    <h2 class="slide__title" style="color: #213360;">Dr. Alexander Bell</h2>
+                    <p class="slide__desc" style="color: #213360;">"Kesehatan Utama, Layanan Terbaik"</p>
+                    <div class="d-flex flex-wrap justify-content-center align-items-center">
+                        <a href="#" class="btn btn__white btn__rounded mr-30">
+                        <span>Buat Janji Temu Doctors</span>
+                        <i class="icon-arrow-right"></i>
+                        </a>
+                    </div>
+                    </div><!-- /.slide-content -->
+                </div><!-- /.col-xl-7 -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+            </div><!-- /.slide-item -->
+        </div><!-- /.carousel -->
+        </section><!-- /.slider -->
+
+        <!-- ========================
+        Jadwal Pelayanan 
+        =========================== -->
+        <section class="shop-grid">
+        <div class="container">
+            <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-6 offset-lg-3">
+                <div class="heading text-center mb-40">
+                <h3 class="heading__title">Jadwal Pelayanan</h3>
+                </div><!-- /.heading -->
+            </div><!-- /.col-lg-6 -->
+            </div>
+            <br><!-- /.row -->
+            <!-- ============================
+                                contact info
+                            ============================== -->
+            <section class="contact-info py-0">
+            <div class="container">
+                <div class="row row-no-gutter boxes-wrapper">
+                <div class="col-sm-12 col-md-4">
+                    <div class="contact-box d-flex">
+                    <div class="contact__content">
+                        <h6 class="heading__title">RSUD Cisalak</h6>
+                        <h1 class="contact__title">Senin - Jumat</h1>
+                        <h1 class="contact__title">08.00 - 13.00</h1>
+                    </div><!-- /.contact__content -->
+                    </div><!-- /.contact-box -->
+                </div><!-- /.col-md-4 -->
+                <div class="col-sm-12 col-md-4">
+                    <div class="contact-box d-flex">
+                    <div class="contact__icon">
+                        <i class="icon-call3"></i>
+                    </div><!-- /.contact__icon -->
+                    <div class="contact__content">
+                        <h2 class="contact__title">Kasus Darurat</h2>
+                        <p class="contact__desc">Jangan ragu untuk menghubungi staf resepsi kami yang ramah dengan pertanyaan umum atau
+                        medis.</p>
+                        <a href="tel:+62812168168" class="phone__number">
+                        <i class="icon-phone"></i> <span>+62812168168</span>
+                        </a>
+                    </div><!-- /.contact__content -->
+                    </div><!-- /.contact-box -->
+                </div><!-- /.col-md-4 -->
+                <div class="col-sm-12 col-md-4">
+                    <div class="contact-box d-flex">
+                    <div class="contact__content">
+                        <h6 class="heading__title">Poli Umum</h6>
+                        <h1 class="contact__title">Senin - Sabtu</h1>
+                        <h1 class="contact__title">13.00 - 18.00</h1>
+                    </div><!-- /.contact__content -->
+                    </div><!-- /.contact-box -->
+                </div><!-- /.col-md-4 -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+            </section><!-- /.contact-info -->
+        </div><!-- /.container -->
+        </section><!-- /.shop -->
+
+
+        <!-- ========================= 
+            Testimonials layout 2
+            =========================  -->
+        <section class="testimonials-layout2 pt-30 pb-40">
+        <div class="container">
+            <div class="testimonials-wrapper">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-5">
+                <div class="heading-layout2">
+                    <img src="assets/images/blog/grid/10.jpg" alt="post image" loading="lazy">
+                </div><!-- /.heading -->
+                </div><!-- /.col-lg-5 -->
+                <div class="col-sm-12 col-md-12 col-lg-7">
+                <div class="slider-with-navs">
+                    <!-- Testimonial #1 -->
+                    <div class="testimonial-item">
+                    <h3 class="heading__title">Layanan Di Rumah</h3>
+                    <h6 class="contact__desc" style="text-align: justify;">Pelayanan dokter dengan jadwal pelayanan setiap hari senin s.d sabtu, 15.00 - 19.00
+                    </h6>
+                    <h6 class="contact__desc" style="text-align: justify;">Pelayanan yang diberikan meliputi konsultasi kesehatan, pemeriksaan fisik, pemberian resep obat, dan edukasi kesehatan
+                    terkait penyakit yang diderita, cara penanganan, juga pencegahannya.</h6>
+                    <h6 class="contact__desc">Tarif konsultasi dan pemeriksaan dokter umum sebesar Rp. 45.000,- di luar biaya obat yang diresepkan dokter menyesuaikan
+                    kebutuhan pasien</h6>
+                    </div><!-- /. testimonial-item -->
+                </div><!-- /.slick-carousel -->
+                </div><!-- /.col-lg-7 -->
+            </div><!-- /.row -->
+            </div><!-- /.testimonials-wrapper -->
+        </div><!-- /.container -->
+        </section><!-- /.testimonials layout 2 -->
+
+        <section class="testimonials-layout2 pt-50 pb-40">
+        <div class="container">
+            <div class="testimonials-wrapper">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-7">
+                <div class="slider-with-navs">
+                    <!-- Testimonial #1 -->
+                    <div class="testimonial-item">
+                    <h3 class="heading__title">Layanan BPJS</h3>
+                    <h6 class="contact__desc" style="text-align: justify;">Klinik Pratama DR ALEX memberikan pelayanan kesehatan bagi peserta BPJS. Pelayanan kesehatan tersebut meliputi pelayanan
+                    dokter umum dan laboratorium sederhana. Keunggulan pelayanan BPJS di klinik kami adalah:
+                    </h6>
+                    <h6 class="contact__desc">1. Jam pelayanan panjang. <br>
+                    2. Tidak membedakan jam pelayanan bagi pasien umum dan BPJS. <br>
+                    3. Admin dan dokter ramah dan komunikatif. <br>
+                    4. Gratis konsultasi online di mana saja dan kapan saja.</h6>
+                    </div><!-- /. testimonial-item -->
+                </div><!-- /.slick-carousel -->
+                </div><!-- /.col-lg-7 -->
+                <div class="col-sm-12 col-md-12 col-lg-5">
+                <div class="heading-layout2">
+                    <img src="assets/images/blog/grid/10.jpg" alt="post image" loading="lazy">
+                </div><!-- /.heading -->
+                </div><!-- /.col-lg-5 --> 
+            </div><!-- /.row -->
+            </div><!-- /.testimonials-wrapper -->
+        </div><!-- /.container -->
+        </section><!-- /.testimonials layout 2 -->
+
+        <br><br> <br><br>
+
+        <!-- ======================
+                Blog Grid
+                ========================= -->
+        <section class="blog-grid" style="margin-top: -150px;">
+        <div class="container">
+            <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-6 offset-lg-3">
+                <div class="heading text-center mb-40">
+                <h3 class="heading__title">Artikel Kesehatan</h3>
+                </div><!-- /.heading -->
+            </div><!-- /.col-lg-6 -->
+            </div><!-- /.row -->
+            <div class="row">
+            <!-- Post Item #1 -->
+            <div class="col-sm-12 col-md-6 col-lg-4">
+                <div class="post-item">
+                <div class="post__img">
+                    <a
+                    href="https://www.cnnindonesia.com/gaya-hidup/20230607122324-255-958704/waspada-kualitas-udara-buruk-dokter-ingatkan-tetap-pakai-masker">
+                    <img src="assets/images/blog/grid/7.jpg" alt="post image" loading="lazy">
+                    </a>
+                </div><!-- /.post__img -->
+                <div class="post__body">
+                    <div class="post__meta-cat">
+                    <a href="#">Kesehatan Udara</a>
+                    </div><!-- /.blog-meta-cat -->
+                    <div class="post__meta d-flex">
+                    <span class="post__meta-date">Juni 7, 2023</span>
+                    <a class="post__meta-author" href="#">CNN Indonesia</a>
+                    </div>
+                    <h4 class="post__title"><a
+                        href="https://www.cnnindonesia.com/gaya-hidup/20230607122324-255-958704/waspada-kualitas-udara-buruk-dokter-ingatkan-tetap-pakai-masker">Waspada
+                        Kualitas Udara Buruk, Dokter Ingatkan Tetap Pakai Masker</a></h4>
+        
+                    <p class="post__desc" style="text-align: justify;">Dokter spesialis paru di RSUP Persahabatan, Erlina Burhan
+                    menyebut kualitas udara di Jakarta dan sekitarnya memang
+                    berada di level yang mengkhawatirkan. Buruknya kualitas udara ini tidak hanya memengaruhi kondisi
+                    kesehatan anak-anak,
+                    bahkan orang dewasa juga bisa terdampak. Saat ini banyak masyarakat yang sudah melepas masker baik saat
+                    beraktivitas di dalam maupun di luar ruangan.
+                    </p>
+                    <a href="https://www.cnnindonesia.com/gaya-hidup/20230607122324-255-958704/waspada-kualitas-udara-buruk-dokter-ingatkan-tetap-pakai-masker"
+                    class="btn btn__secondary btn__link btn__rounded">
+                    <span>Read More</span>
+                    <i class="icon-arrow-right"></i>
+                    </a>
+                </div><!-- /.post__body -->
+                </div><!-- /.post-item -->
+            </div><!-- /.col-lg-4 -->
+            <!-- Post Item #2 -->
+            <div class="col-sm-12 col-md-6 col-lg-4">
+                <div class="post-item">
+                <div class="post__img">
+                    <a
+                    href="https://www.cnnindonesia.com/gaya-hidup/20230530103037-262-955646/wajib-tahu-8-makanan-yang-bisa-menurunkan-kadar-gula-darah">
+                    <img src="assets/images/blog/grid/8.jpg" alt="post image" loading="lazy">
+                    </a>
+                </div><!-- /.post__img -->
+                <div class="post__body">
+                    <div class="post__meta-cat">
+                    <a href="#">Kesehatan Makanan</a>
+                    </div><!-- /.blog-meta-cat -->
+                    <div class="post__meta d-flex">
+                    <span class="post__meta-date">Juni 5, 2023</span>
+                    <a class="post__meta-author" href="#">CNN Indonesia</a>
+                    </div>
+                    <h4 class="post__title"><a
+                        href="https://www.cnnindonesia.com/gaya-hidup/20230530103037-262-955646/wajib-tahu-8-makanan-yang-bisa-menurunkan-kadar-gula-darah">Wajib
+                        Tahu, 8 Makanan yang Bisa Menurunkan Kadar Gula Darah</a></h4>
+                    <p class="post__desc" style="text-align: justify;">Memilih makanan dengan indeks glikemik (GI) rendah adalah
+                    salah satu cara untuk membantu menurunkan atau mengelola kadar
+                    gula darah. Indeks glikemik mengukur efek makanan tertentu pada kadar gula darah. Bagi penderita diabetes,
+                    makanan dan minuman yang diserap tubuh secara perlahan adalah yang terbaik karena tidak
+                    menyebabkan lonjakan dan penurunan gula darah.
+                    </p>
+                    <a href="https://www.cnnindonesia.com/gaya-hidup/20230530103037-262-955646/wajib-tahu-8-makanan-yang-bisa-menurunkan-kadar-gula-darah"
+                    class="btn btn__secondary btn__link btn__rounded">
+                    <span>Read More</span>
+                    <i class="icon-arrow-right"></i>
+                    </a>
+                </div><!-- /.post__body -->
+                </div><!-- /.post-item -->
+            </div><!-- /.col-lg-4 -->
+            <!-- Post Item #3 -->
+            <div class="col-sm-12 col-md-6 col-lg-4">
+                <div class="post-item">
+                <div class="post__img">
+                    <a
+                    href="https://www.cnnindonesia.com/gaya-hidup/20230530173747-255-955933/udara-jakarta-lagi-tak-sehat-waspada-5-penyakit-akibat-polusi-ini">
+                    <img src="assets/images/blog/grid/9.jpg" alt="post image" loading="lazy">
+                    </a>
+                </div><!-- /.post__img -->
+                <div class="post__body">
+                    <div class="post__meta-cat">
+                    <a href="#">Kesehatan Udara</a>
+                    </div><!-- /.blog-meta-cat -->
+                    <div class="post__meta d-flex">
+                    <span class="post__meta-date">Juni 1, 2023</span>
+                    <a class="post__meta-author" href="#">CNN Indonesia</a>
+                    </div>
+                    <h4 class="post__title"><a
+                        href="https://www.cnnindonesia.com/gaya-hidup/20230530173747-255-955933/udara-jakarta-lagi-tak-sehat-waspada-5-penyakit-akibat-polusi-ini">Udara
+                        Jakarta Lagi Tak Sehat, Waspada 5 Penyakit Akibat Polusi Ini</a></h4>
+        
+                    <p class="post__desc" style="text-align: justify;">Sumber polutan di Jakarta masih PM2,5, yakni partikel
+                    udara berukuran lebih kecil dari 2,5 mikron (mikrometer). Mengutip
+                    situs BMKG, nilai ambang batas (NAB) polusi udara yang diperbolehkan berada dalam udara ambien yakni 65
+                    µgram/m3. Namun pada Selasa (30/5), nilai PM2,5 Jakarta mencapai 66,2 µg/m³ atau 13,2 kali batas nilai
+                    pedoman kualitas udara
+                    tahunan Organisasi Kesehatan Dunia (WHO).
+                    </p>
+                    <a href="https://www.cnnindonesia.com/gaya-hidup/20230530173747-255-955933/udara-jakarta-lagi-tak-sehat-waspada-5-penyakit-akibat-polusi-ini"
+                    class="btn btn__secondary btn__link btn__rounded">
+                    <span>Read More</span>
+                    <i class="icon-arrow-right"></i>
+                    </a>
+                </div><!-- /.post__body -->
+                </div><!-- /.post-item -->
+            </div><!-- /.col-lg-4 -->
+            </div><!-- /.row -->
+            <div class="row">
+            <div class="col-12 text-center">
+                <a href="https://jovee.id/artikel/" class="btn btn__secondary btn__rounded">
+                <span>Lihat Selengkapnya</span>
+                <i class="icon-arrow-right"></i>
+                </a>
+            </div><!-- /.col-lg-12 -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </section><!-- /.blog Grid -->
+
+@endsection
+
+@section('container')
+    
+@endsection
---
+++ resources/views/web/profile.blade.php
@@ -0,0 +1,144 @@
+@extends('layouts.mainweb')
+
+@section('content')
+        
+<!-- =========================
+            Header
+        =========================== -->
+    <header class="header header-layout1">
+        <!-- /.header-top -->
+        <nav class="navbar navbar-expand-lg sticky-navbar">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="{{ url('/home') }}">
+                <img src="assets/images/logo/2.png" class="logo-light" alt="logo">
+                <img src="assets/images/logo/1.png" class="logo-dark" alt="logo">
+            </a>
+            <button class="navbar-toggler" type="button">
+                <span class="menu-lines"><span></span></span>
+            </button>
+            <div class="collapse navbar-collapse" id="mainNavigation">
+                <ul class="navbar-nav ml-auto">
+                <li class="nav__item">
+                    <a href="{{ url('/home') }}" class="nav__item-link">Home</a>
+                </li>
+                <!-- /.nav-item -->
+                <li class="nav__item">
+                    <a href="{{ url('/layanan') }}" class="nav__item-link">Layanan</a>
+                </li><!-- /.nav-item -->
+                <li class="nav__item">
+                    <a href="{{ url('/profile') }}" class="nav__item-link active">Profile Doctors</a>
+                </li><!-- /.nav-item -->
+                <li class="nav__item">
+                    <a href="{{ url('/contact') }}" class="nav__item-link">Contacts Us</a>
+                </li><!-- /.nav-item -->
+                </ul><!-- /.navbar-nav -->
+                <button class="close-mobile-menu d-block d-lg-none"><i class="fas fa-times"></i></button>
+            </div><!-- /.navbar-collapse -->
+            <div class="d-none d-xl-flex align-items-center position-relative ml-30">
+                <a href="{{ url('/login') }}" class="btn btn__primary btn__rounded ml-30">
+                <i class="icon-calendar"></i>
+                <span>Login</span>
+                </a>
+            </div>
+            </div><!-- /.container -->
+        </nav><!-- /.navabr -->
+        </header><!-- /.Header -->
+
+        <!-- ========================
+        page title 
+        =========================== -->
+        <section class="page-title page-title-layout5">
+        <div class="bg-img"><img src="assets/images/backgrounds/6.jpg" alt="background"></div>
+        <div class="container">
+            <div class="row">
+            <div class="col-12">
+                <h1 class="pagetitle__heading">Dr. Alexander Bell</h1>
+            </div><!-- /.col-12 -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+        </section><!-- /.page-title -->
+
+        <section class="pt-120 pb-80">
+        <div class="container">
+            <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-4">
+                <aside class="sidebar has-marign-right">
+                <div class="widget widget-member">
+                    <div class="member mb-0">
+                    <div class="member__img">
+                        <img src="assets/images/team/10.jpg" alt="member img">
+                    </div><!-- /.member-img -->
+                    <div class="member__info">
+                        <h5 class="member__name"><a href="{{ url('/profile') }}">Dr. Alexander Bell</a></h5>
+                        <p class="member__job">Spesialis Umum</p>
+                        <p class="member__desc">Dr. Alexander Bell adalah seorang dokter umum yang berdedikasi dan berpengalaman. Ia memiliki pengetahuan luas dalam
+                        berbagai kondisi medis dan memberikan perawatan yang berkualitas kepada pasien-pasienya.</p>
+                        <div class="mt-20 d-flex flex-wrap justify-content-between align-items-center">
+                        <ul class="social-icons list-unstyled mb-0">
+                            <li><a href="#"><i class="fa fa-envelope"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                        </ul><!-- /.social-icons -->
+                        </div>
+                    </div><!-- /.member-info -->
+                    </div><!-- /.member -->
+                </div><!-- /.widget-member -->
+                </aside><!-- /.sidebar -->
+            </div><!-- /.col-lg-4 -->
+            <div class="col-sm-12 col-md-12 col-lg-8">
+                <div class="text-block mb-50">
+                <h5 class="text-block__title">Biography</h5>
+                <p class="text-block__desc mb-20 font-weight-bold color-secondary" style="text-align: justify;">Dr. Alexander Bell adalah seorang dokter umum yang berdedikasi dalam memberikan pelayanan kesehatan kepada masyarakat.
+                Ia tumbuh di lingkungan keluarga yang menghargai kesehatan dan memberikan perhatian pada kesejahteraan orang lain. Sejak kecil, Dr. Alexander Bell tertarik pada ilmu kedokteran dan memiliki semangat untuk membantu orang-orang yang
+                sakit. Setelah menyelesaikan pendidikan dasar dan menengahnya, ia melanjutkan studi di sekolah kedokteran terkemuka di
+                negaranya. Ia menunjukkan dedikasi dan kecerdasan yang luar biasa dalam studinya, dan segera menonjol di antara
+                rekan-rekannya.</p>
+                <p class="text-block__desc mb-20" style="text-align: justify;">Setelah menyelesaikan pendidikan kedokteran, Dr. Alexander Bell memulai praktiknya sebagai dokter umum. Ia berkomitmen
+                untuk memberikan perawatan medis yang berkualitas kepada pasien-pasiennya. Dalam praktik sehari-harinya, ia menghadapi
+                berbagai kondisi medis yang beragam dan berusaha memberikan diagnosis yang akurat serta perawatan yang efektif.</p>
+                <p class="text-block__desc mb-20" style="text-align: justify;">Dr. Alexander Bell juga sadar bahwa pendidikan dan pencegahan merupakan bagian penting dari perawatan kesehatan. Oleh
+                karena itu, ia aktif dalam melakukan kampanye edukasi kesehatan di komunitasnya. Ia memberikan ceramah, menyediakan
+                informasi kesehatan, dan bekerja sama dengan lembaga sosial untuk meningkatkan kesadaran tentang pentingnya menjaga
+                kesehatan.</p>
+                <p class="text-block__desc mb-20" style="text-align: justify;">Selama karirnya sebagai dokter umum, Dr. Alexander Bell terus berusaha meningkatkan pengetahuannya dengan mengikuti
+                pelatihan dan seminar medis terkini. Ia mengikuti perkembangan terbaru dalam bidang kedokteran dan menerapkan
+                penemuan-penemuan baru tersebut dalam prakteknya.</p>
+                </div><!-- /.text-block -->
+                <ul class="details-list list-unstyled mb-60">
+                <li>
+                    <h5 class="details__title">Spesialis</h5>
+                    <div class="details__content">
+                    <p class="mb-0">Umum</p>
+                    </div>
+                </li>
+                <li>
+                    <h5 class="details__title">Kondisi dan Minat Klinis</h5>
+                    <div class="details__content">
+                    <ul class="list-items list-items-layout2 list-unstyled mb-0">
+                        <li>Pemeriksaan Fisik</li>
+                        <li>Pengobatan Darurat</li>
+                    </ul>
+                    </div>
+                </li>
+                <li>
+                    <h5 class="details__title">Prestasi & Penghargaan</h5>
+                    <div class="details__content">
+                    <p class="mb-0">"2015" Ikatan Dokter Indonesia (IDI)</p>
+                    </div>
+                </li>
+                <li>
+                    <h5 class="details__title">Pendidikan</h5>
+                    <div class="details__content">
+                    <p class="mb-0">"2015" Universitas UPN Veteran Jakarta - Kedokteran Umum</p>
+                    </div>
+                </li>
+                </ul>
+            </div><!-- /.col-lg-8 -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </section>
+
+
+@endsection
+@section('container')
+    
+@endsection
