229b8b221c32b7f06a89c6c4a5314394d2dd46b7
Akhmad Ramadani <akhmdramadani18@gmail.com>
2023-11-21 12:05:34 +0700

    fix announcement controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers;
+use App\Http\Controllers\Api\AnnouncementController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\DoctorController;
 use App\Http\Controllers\Api\MedicalRecordsController;
