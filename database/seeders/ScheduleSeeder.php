c3a8e102042d3bcfea926ce61feea18583e203f3
olvinyauri <yauri990@gmail.com>
2023-12-14 22:19:40 +0700

    seed

--- database/seeders/ScheduleSeeder.php
+++ database/seeders/ScheduleSeeder.php
@@ -19,36 +19,7 @@ class ScheduleSeeder extends Seeder
         $now = Carbon::now();
 
         $schedules = [
-            [
-                'employee_id' => 1,
-                'place_id' => 2,
-                'schedule_date' => $today->toDateString(),
-                'schedule_time' => $now->toTimeString(),
-                'schedule_time_end' => $now->addHours(3)->toTimeString(),
-                'qty' => 10,
-                'created_at' => $now,
-                'updated_at' => $now,
-            ],
-            [
-                'employee_id' => 1,
-                'place_id' => 2,
-                'schedule_date' => $today->toDateString(),
-                'schedule_time' => $now->addHours(4)->toTimeString(),
-                'schedule_time_end' => $now->addHours(7)->toTimeString(),
-                'qty' => 10,
-                'created_at' => $now,
-                'updated_at' => $now,
-            ],
-            [
-                'employee_id' => 1,
-                'place_id' => 1,
-                'qty' => 0,
-                'schedule_date' => $today->addDays(1)->toDateString(),
-                'schedule_time' => $now->toTimeString(),
-                'schedule_time_end' => $now->toTimeString(),
-                'created_at' => $now,
-                'updated_at' => $now,
-            ],
+            
         ];
         DB::table('schedules')->insert($schedules);
     }
