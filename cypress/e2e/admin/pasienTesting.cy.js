629ffc634c20fe49e95df202f0c561edf2e7a392
Moch Fajrul Falah <mfajrulfalah.ff@gmail.com>
2024-06-15 17:58:42 +0700

    uncomment

--- cypress/e2e/admin/pasienTesting.cy.js
+++ cypress/e2e/admin/pasienTesting.cy.js
@@ -14,100 +14,100 @@ describe("pasien testing", () => {
         cy.visit("http://127.0.0.1:8000/admin/pasien");
     });
 
-    // it("see list of pasien", () => {
-    //   cy.contains("Nama Pasien").should("exist");
-    // });
+    it("see list of pasien", () => {
+      cy.contains("Nama Pasien").should("exist");
+    });
 
-    // it("add new pasien valid", () => {
-    //   cy.get(".button-action > .btn").click();
-    //   cy.get('#namapasien').type("Pasien Baru");
-    //   cy.get('#tanggallahir').type("2000-01-01");
-    //   cy.get(':nth-child(4) > .form-control').select(1);
-    //   cy.get('#email').type("pasienBaru@mail.com");
-    //   cy.get('#alamat').type("Jl. Pasien Baru");
-    //   cy.get(':nth-child(7) > .col > .form-group > #nomorhandphone').type("081234567890");
-    //   cy.get(':nth-child(8) > .col > .form-group > #nomorhandphone').type("80");
-    //   cy.get(':nth-child(9) > .col > .form-group > #nomorhandphone').type('200');
-    //   cy.get('#username').type("PasienBaru");
-    //   cy.get('#password').type('12345');
-    //   cy.get('.btn-primary').click();
-    //   cy.contains('Patient created successfully').should('exist');
-    // });
+    it("add new pasien valid", () => {
+      cy.get(".button-action > .btn").click();
+      cy.get('#namapasien').type("Pasien Baru");
+      cy.get('#tanggallahir').type("2000-01-01");
+      cy.get(':nth-child(4) > .form-control').select(1);
+      cy.get('#email').type("pasienBaru@mail.com");
+      cy.get('#alamat').type("Jl. Pasien Baru");
+      cy.get(':nth-child(7) > .col > .form-group > #nomorhandphone').type("081234567890");
+      cy.get(':nth-child(8) > .col > .form-group > #nomorhandphone').type("80");
+      cy.get(':nth-child(9) > .col > .form-group > #nomorhandphone').type('200');
+      cy.get('#username').type("PasienBaru");
+      cy.get('#password').type('12345');
+      cy.get('.btn-primary').click();
+      cy.contains('Patient created successfully').should('exist');
+    });
 
-    // it("add new pasien invalid empty input", () => {
-    //   cy.get(".button-action > .btn").click();
-    //   cy.get("#namapasien:invalid")
-    //       .invoke("prop", "validationMessage")
-    //       .should("contain", "Please fill in this field");;
-    // });
+    it("add new pasien invalid empty input", () => {
+      cy.get(".button-action > .btn").click();
+      cy.get("#namapasien:invalid")
+          .invoke("prop", "validationMessage")
+          .should("contain", "Please fill in this field");;
+    });
 
-    // it("add new pasien invalid tanggal lahir more than today", () => {
-    //   cy.get(".button-action > .btn").click();
-    //       cy.get("#tanggallahir").type("2025-05-05");
-    //     cy.get(".btn-primary").click();
-    //     cy.contains("Tanggal lahir tidak boleh lebih dari hari ini").should(
-    //         "exist"
-    //     );
-    // });
+    it("add new pasien invalid tanggal lahir more than today", () => {
+      cy.get(".button-action > .btn").click();
+          cy.get("#tanggallahir").type("2025-05-05");
+        cy.get(".btn-primary").click();
+        cy.contains("Tanggal lahir tidak boleh lebih dari hari ini").should(
+            "exist"
+        );
+    });
 
-    // it("add new pasien invalid same email", () => {
-    //     cy.get(".button-action > .btn").click();
-    //       cy.get('#namapasien').type("Pasien Baru");
-    //       cy.get('#tanggallahir').type("2000-01-01");
-    //       cy.get(':nth-child(4) > .form-control').select(1);
-    //       cy.get('#email').type("pasienBaru@mail.com");
-    //       cy.get('#alamat').type("Jl. Pasien Baru");
-    //       cy.get(':nth-child(7) > .col > .form-group > #nomorhandphone').type("081234567890");
-    //       cy.get(':nth-child(8) > .col > .form-group > #nomorhandphone').type("80");
-    //       cy.get(':nth-child(9) > .col > .form-group > #nomorhandphone').type('200');
-    //       cy.get('#username').type("PasienBaru");
-    //       cy.get('#password').type('12345');
-    //       cy.get('.btn-primary').click();
-    //       cy.contains("The email has already been registered").should("exist");
-    // });
+    it("add new pasien invalid same email", () => {
+        cy.get(".button-action > .btn").click();
+          cy.get('#namapasien').type("Pasien Baru");
+          cy.get('#tanggallahir').type("2000-01-01");
+          cy.get(':nth-child(4) > .form-control').select(1);
+          cy.get('#email').type("pasienBaru@mail.com");
+          cy.get('#alamat').type("Jl. Pasien Baru");
+          cy.get(':nth-child(7) > .col > .form-group > #nomorhandphone').type("081234567890");
+          cy.get(':nth-child(8) > .col > .form-group > #nomorhandphone').type("80");
+          cy.get(':nth-child(9) > .col > .form-group > #nomorhandphone').type('200');
+          cy.get('#username').type("PasienBaru");
+          cy.get('#password').type('12345');
+          cy.get('.btn-primary').click();
+          cy.contains("The email has already been registered").should("exist");
+    });
 
-    // it("edit pasien valid", () => {
-    //     cy.get(
-    //         ":nth-child(1) > .project-actions > form > .btn-warning"
-    //     ).click();
-    //     cy.get("#namapasien").clear().type("Pasien Lama");
-    //     cy.get("#tanggallahir").type("2000-05-05");
-    //     cy.get('.btn-primary').click();
-    //     cy.contains("Patient updated successfully").should("exist");
-    // });
+    it("edit pasien valid", () => {
+        cy.get(
+            ":nth-child(1) > .project-actions > form > .btn-warning"
+        ).click();
+        cy.get("#namapasien").clear().type("Pasien Lama");
+        cy.get("#tanggallahir").type("2000-05-05");
+        cy.get('.btn-primary').click();
+        cy.contains("Patient updated successfully").should("exist");
+    });
 
-    // it("edit pasien tanggal lahir more than today", () => {
-    //     cy.get(
-    //         ":nth-child(1) > .project-actions > form > .btn-warning"
-    //     ).click();
-    //     cy.get("#tanggallahir").type("2025-05-05");
-    //     cy.get('.btn-primary').click();
-    //     cy.contains(
-    //         "Tanggal lahir tidak boleh lebih dari hari ini"
-    //     ).should("exist");
-    // });
+    it("edit pasien tanggal lahir more than today", () => {
+        cy.get(
+            ":nth-child(1) > .project-actions > form > .btn-warning"
+        ).click();
+        cy.get("#tanggallahir").type("2025-05-05");
+        cy.get('.btn-primary').click();
+        cy.contains(
+            "Tanggal lahir tidak boleh lebih dari hari ini"
+        ).should("exist");
+    });
 
-    // it("delete pasien", () => {
-    //     cy.get(":nth-child(1) > .project-actions > form > .btn-danger").click();
-    //     cy.contains(
-    //         "Patient successfully deleted"
-    //     ).should("exist");
-    // });
+    it("delete pasien", () => {
+        cy.get(":nth-child(1) > .project-actions > form > .btn-danger").click();
+        cy.contains(
+            "Patient successfully deleted"
+        ).should("exist");
+    });
 
-    // it("see pasien reservation", () => {
-    //   cy.get(":nth-child(1) > .project-actions > form > .show-reservations").click();
-    //     cy.contains(
-    //         "Dokter"
-    //     ).should("exist");
-    // });
+    it("see pasien reservation", () => {
+      cy.get(":nth-child(1) > .project-actions > form > .show-reservations").click();
+        cy.contains(
+            "Dokter"
+        ).should("exist");
+    });
 
-    // it("change password user", () => {
-    //     cy.get(".odd > .project-actions > form > .btn-primary").click();
-    //     cy.get(":nth-child(1) > .form-group > .form-control").type("12345");
-    //     cy.get(":nth-child(2) > .form-group > .form-control").type("12345");
-    //     cy.get(".btn").click();
-    //     cy.contains("Password changed successfully").should("exist");
-    // });
+    it("change password user", () => {
+        cy.get(".odd > .project-actions > form > .btn-primary").click();
+        cy.get(":nth-child(1) > .form-group > .form-control").type("12345");
+        cy.get(":nth-child(2) > .form-group > .form-control").type("12345");
+        cy.get(".btn").click();
+        cy.contains("Password changed successfully").should("exist");
+    });
 
     it("change password user not match", () => {
         cy.get(".odd > .project-actions > form > .btn-primary").click();
